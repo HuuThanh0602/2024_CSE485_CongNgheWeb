a305b69ba2eca67b66886eb2beaa36a900fd9ea6
Nguyễn Thái Sơn <khoaii1604@gmail.com>
2024-09-26 22:50:43 +0700

    Update list_category.php

--- BTTH02/views/category/list_category.php
+++ BTTH02/views/category/list_category.php
@@ -1,5 +1,5 @@
 <?php
-include_once($_SERVER['DOCUMENT_ROOT'] . "/CSE485/BTTH02/services/CategoryService.php");
+include_once("./services/CategoryService.php");
 $categoryService = new CategoryService();
 $categories = $categoryService->getAllCategories();
 ?>
