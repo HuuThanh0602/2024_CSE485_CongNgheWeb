5cdd06ae6fb4f9eaf4824f4f4aa371b477fcfd67
HuuThanh0602 <nguyenhuuthanh06022004@gmail.com>
2024-09-26 22:03:53 +0700

    update

--- BTTH02/views/article/add_article.php
+++ BTTH02/views/article/add_article.php
@@ -15,7 +15,7 @@
         <div class="row">
             <div class="col-sm">
                 <h3 class="text-center text-uppercase fw-bold">Thêm mới bài viết</h3>
-                <form action="../BTTH02/index.php?controller=Article&action=addArticle" method="post">
+                <form action="./index.php?controller=Article&action=addArticle" method="post">
                     <div class="input-group mt-3 mb-3">
                         <span class="input-group-text" id="lblTitleName">Tên tiêu đề </span>
                         <input type="text" class="form-control" name="txtTitle" >
@@ -59,7 +59,7 @@
 
                     <div class="form-group  float-end ">
                         <input type="submit" value="Thêm" class="btn btn-success">
-                        <a href="../BTTH02/index.php?controller=Article&action=list" class="btn btn-warning ">Quay lại</a>
+                        <a href="./index.php?controller=Article&action=list" class="btn btn-warning ">Quay lại</a>
                     </div>
                     
                 </form>
