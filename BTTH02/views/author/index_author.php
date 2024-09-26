747d11e42c92524cf23ddcfae54dedf34b287c05
Nguyễn Thái Sơn <khoaii1604@gmail.com>
2024-09-26 22:45:31 +0700

    Update index_author.php

--- BTTH02/views/author/index_author.php
+++ BTTH02/views/author/index_author.php
@@ -1,5 +1,5 @@
 <?php
-include_once($_SERVER['DOCUMENT_ROOT'] . "/CSE485/BTTH02/services/AuthorService.php");
+include_once("./services/AuthorService.php");
 $authorService = new AuthorService();
 $authors = $authorService->getAllAuthor();
 ?>
